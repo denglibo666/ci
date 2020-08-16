9d5d346b4b758441fa5145597ff1bae998117044
Libo <15023693438@163.com>
2020-08-16 11:40:32 +0800

    learn AJAX

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,6 +8,13 @@ class Home extends BaseController
 		return view('welcome_message');
 	}
 
+    //登陆
+    public function login()
+	{
+		var_dump($_POST['name']);die;
+		return view('welcome_message');
+	}
+
 	//--------------------------------------------------------------------
 
 }
